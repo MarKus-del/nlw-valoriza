49b597ad5e8180c7f959998d2146c2d11ccc9821
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-21 13:57:14 -0300

    começando projeto

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running in http://localhost:${port}`);
+});
