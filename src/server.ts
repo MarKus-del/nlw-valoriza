bd2d4650d5daf8557117541cd0cbfeef36067e3c
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-22 16:41:27 -0300

    criado rota para criar usuarios

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import "reflect-metadata";
 import "./database";
 
+import { router } from './routes';
+
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is running in http://localhost:${port}`);
