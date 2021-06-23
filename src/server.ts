072147141609e8105b1af73e4bac6132f4b87127
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-23 13:31:37 -0300

    adicionando middleware de erro

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction, response } from 'express';
+import "express-async-errors";
 import "reflect-metadata";
 import "./database";
 
@@ -11,6 +12,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running in http://localhost:${port}`);
