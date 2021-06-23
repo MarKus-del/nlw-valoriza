e028900bfb4bc46a6350cbd0d0b20deb48935f93
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-23 13:32:04 -0300

    criada migration de tags

--- src/controller/CreateUserController.ts
+++ src/controller/CreateUserController.ts
@@ -7,15 +7,9 @@ class CreateUserController {
 
     const createUserService = new CreateUserService();
 
-    try {
-      const user = await createUserService.execute({ name, email, admin });
+    const user = await createUserService.execute({ name, email, admin });
 
-      return res.status(201).json(user);
-    } catch (error) {
-      return res.status(400).json({
-        error
-      });
-    }
+    return res.status(201).json(user);
   }
 }
 export { CreateUserController };
---
+++ src/database/migrations/1624411136057-CreateTags.ts
@@ -0,0 +1,38 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTags1624411136057 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'tags',
+                columns: [
+                    {
+                        type: "uuid",
+                        name: "id",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('tags');
+    }
+
+}
